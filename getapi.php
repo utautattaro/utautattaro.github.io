3811f6961eccb47070894b4164348fa7fe6f3ab1
TsudaRyotaro <ryotaro.tsuda.kait.d@gmail.com>
2017-02-17 00:06:50 +0900

    add api

---
+++ getapi.php
@@ -0,0 +1,36 @@
+<html>
+ <head>
+  <title>shortenurl</title>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83861814-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+ </head>
+ <body>
+ <?php
+//gets the data from a URL 
+function get_tiny_url($beforeurl)  {  
+	$ch = curl_init();  
+	$timeout = 5;  
+	curl_setopt($ch,CURLOPT_URL,'http://tinyurl.com/api-create.php?url='.$beforeurl);  
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);  
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);  
+	$data = curl_exec($ch);  
+	curl_close($ch);  
+	return $data;  
+}
+
+$url = $_GET["url"];
+//test it out!
+$new_url = get_tiny_url($url);
+
+echo $new_url;
+?>
+ </body>
+</html>
